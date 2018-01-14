dff30124176706e8610f034ae36b92b7e34e330c
Tim Holman <timothy.w.holman@gmail.com>
2018-01-15 10:35:48 +1100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Header from './components/Header.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <Header />
+
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>The Zen Zone</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
