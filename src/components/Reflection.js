a4b474d52d329ba3f38fc1e0153c4ceb80fdb7ae
Tim Holman <timothy.w.holman@gmail.com>
2018-01-24 15:51:49 +1000

    timing

--- src/components/Reflection.js
+++ src/components/Reflection.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import '../styles/Intro.css';
 import '../styles/Reflection.css';
-// import logo from '../assets/Logo.svg';
+import logo from '../assets/Logo.svg';
 
 class Reflection extends Component {
   render() {
+
+    let totalSeconds = this.props.totalTime / 1000;
+    let totalMinutes = Math.floor(totalSeconds / 60);
+    let remainingSeconds = Math.floor(totalSeconds % 60);
+
     return (
-      <section className="center">
-        Nice, you have been meditating for X minutes and Y seconds
-        <button onClick={this.props.onButtonClick}>Lets do some more</button>
-      </section>
+      <header className="center intro-component">
+        <img src={logo} className="App-logo" alt="The Zen Zone" />
+        <div className="zen-zone-intro">
+          <h1>Nice :)</h1>
+          <p>
+            You have been meditating with us for <b>{totalMinutes}</b> minutes and <b>{remainingSeconds}</b> seconds.
+          </p>
+          <button onClick={this.props.onButtonClick}>Lets do some more?</button>
+        </div>
+      </header>
     );
   }
 }
